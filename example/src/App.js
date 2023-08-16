aac8d4c9b720cced03456e7bd4b1c0f8c8046775
Lulu Tung <lulutheflaneur@gmail.com>
2023-08-16 14:24:13 +0800

    Add TreeSelect example

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,60 @@
-import {InputMui} from "@lulutheflaneur/mui-custom-components";
+import {InputMui, RowMui, TreeSelectMui} from "@lulutheflaneur/mui-custom-components";
+import {Grid} from "@mui/material";
+import {useState} from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>hi
-                </p>
-                <InputMui label="hi"/>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    )
+
+    const [schoolClass, setSchoolClass] = useState('')
+
+    const setValue = (_, classId) => {
+        setSchoolClass(classId);
+    };
+
+    const optionTree = [{
+        children_data: [{code: '1a', level: 2, nodeLabel: 'Class 1A'},
+            {code: '1b', level: 2, nodeLabel: 'Class 1B'},
+            {code: '1c', level: 2, nodeLabel: 'Class 1C'}],
+        code: 'p1',
+        disabled: true,
+        level: 1,
+        nodeLabel: 'Primary 1'
+    }]
+
+    return (<div className="App">
+        <header className="App-header">
+            <p>Sample lunch box ordering form using MUI Custom Component library
+            </p>
+        </header>
+        <body>
+        <RowMui>
+            <RowMui justifyContent="center">
+                <Grid item xs={6}>
+                    <InputMui label="name" InputLabelProps={{shrink: true}}/>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <InputMui label="age" InputLabelProps={{shrink: true}}
+                    />
+                </Grid>
+            </RowMui>
+            <RowMui justifyContent="center">
+                {/*<Grid item xs={6}>*/}
+                <TreeSelectMui
+                    defaultExpandAll
+                    InputLabelProps={{shrink: true}}
+                    label="Class"
+                    labelKey="nodeLabel"
+                    name="schoolClass"
+                    nestedTree={optionTree}
+                    valueKey="code"
+                    setValue={setValue}
+                    uniqueKey="code"
+                />
+                {/*</Grid>*/}
+            </RowMui>
+        </RowMui>
+        </body>
+    </div>)
 }
 
 export default App
