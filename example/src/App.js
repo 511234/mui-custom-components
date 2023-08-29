19c76a74105f09bd3d75f70ff564326b970c4c1b
Lulu Tung <lulutheflaneur@gmail.com>
2023-08-29 18:46:02 -0400

    Finish demo

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,48 @@
-import {ButtonMui, CheckboxMui, InputMui, RowMui, TreeSelectMui} from "@lulutheflaneur/mui-custom-components";
+import {
+    ButtonMui,
+    CheckboxMui,
+    InputMui,
+    RowMui,
+    SelectMui,
+    SnackbarMui,
+    SpanMui,
+    TreeSelectMui
+} from "@lulutheflaneur/mui-custom-components";
 import {Grid} from "@mui/material";
 import {useRef, useState} from "react";
 
 function App() {
 
+    const [isSuccess, setIsSuccess] = useState(false);
     const [schoolClass, setSchoolClass] = useState('')
+    const [lunchBox, setLunchBox] = useState('')
 
+    const snackbarRef = useRef(null)
     const nameRef = useRef(null);
     const ageRef = useRef(null)
     const returningStudentRef = useRef(null)
 
+    const handleLunchboxChange = (e) => {
+        setLunchBox(e.target.value)
+    }
+
     const handleSubmit = () => {
+        snackbarRef.current.handleOpen()
+
+        if (!nameRef.current.value || !ageRef.current.value || !schoolClass || !lunchBox) {
+            setIsSuccess(false)
+            snackbarRef.current.setMessage('Please fill in all fields')
+            return;
+        }
+
+        setIsSuccess(true)
+        snackbarRef.current.setMessage('Successfully submitted form')
         console.log('-------------------------------')
         console.log('Name: ', nameRef.current.value)
         console.log('Age: ', ageRef.current.value)
         console.log('Class: ', schoolClass)
         console.log('Is returning student: ', returningStudentRef.current.checked)
+        console.log('Lunch box: ', lunchBox)
         console.log('-------------------------------')
 
     }
@@ -24,54 +51,95 @@ function App() {
         setSchoolClass(classId);
     };
 
-    const optionTree = [{
-        children_data: [{code: '1a', level: 2, nodeLabel: 'Class 1A'},
-            {code: '1b', level: 2, nodeLabel: 'Class 1B'},
-            {code: '1c', level: 2, nodeLabel: 'Class 1C'}],
-        code: 'p1',
-        disabled: true,
-        level: 1,
-        nodeLabel: 'Primary 1'
-    }]
+    const lunchOptions = [
+        {label: 'Lunch A: Pan seared halibut with asparagus', value: 'a'},
+        {label: 'Lunch B: Banana waffles with hash brown and sausages', value: 'b'},
+        {label: 'Lunch C: Chicken fried rice', value: 'c'},
+    ]
 
-    return (<div className="App">
-        <header className="App-header">
-            <p>Sample lunch box ordering form using MUI Custom Component library
-            </p>
-        </header>
-        <Grid container spacing={2} style={{padding: 50}}>
-            <RowMui justifyContent="center">
-                <Grid item xs={6}>
-                    <InputMui label="name" InputLabelProps={{shrink: true}} ref={nameRef}/>
+    const optionTree = [
+        {
+            children_data: [{code: '1a', level: 2, nodeLabel: 'Class 1A'},
+                {code: '1b', level: 2, nodeLabel: 'Class 1B'},
+                {code: '1c', level: 2, nodeLabel: 'Class 1C'}],
+            code: 'p1',
+            disabled: true,
+            level: 1,
+            nodeLabel: 'Year 1'
+        },
+        {
+            children_data: [{code: '2a', level: 2, nodeLabel: 'Class 2A'},
+                {code: '2b', level: 2, nodeLabel: 'Class 2B'},
+                {code: '2c', level: 2, nodeLabel: 'Class 2C'}],
+            code: 'p2',
+            disabled: true,
+            level: 1,
+            nodeLabel: 'Year 2'
+        }
+    ]
+
+    return (
+        <>
+            <div className="App">
+                <header style={{textAlign: 'center'}}>
+                    <SpanMui>
+                        Sample lunch box ordering form using MUI Custom Component library
+                    </SpanMui>
+                </header>
+
+                <Grid container spacing={2} style={{padding: 50}}>
+                    
+                    <RowMui justifyContent="center">
+                        <Grid item xs={6}>
+                            <InputMui label="name" InputLabelProps={{shrink: true}} ref={nameRef}/>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <InputMui label="age" type="number" InputLabelProps={{shrink: true}} ref={ageRef}/>
+                        </Grid>
+                    </RowMui>
+
+                    <RowMui style={{paddingLeft: '1rem'}}>
+                        <TreeSelectMui
+                            defaultExpandAll
+                            InputLabelProps={{shrink: true}}
+                            label="Class"
+                            labelKey="nodeLabel"
+                            name="schoolClass"
+                            nestedTree={optionTree}
+                            valueKey="code"
+                            setValue={setValue}
+                            uniqueKey="code"
+                        />
+                    </RowMui>
+
+                    <RowMui style={{paddingLeft: '1rem'}}>
+                        <SelectMui
+                            InputLabelProps={{shrink: true}}
+                            label="Select Lunchbox"
+                            onChange={handleLunchboxChange}
+                            options={lunchOptions}
+                        />
+                    </RowMui>
+
+                    <RowMui style={{paddingLeft: '1rem'}}>
+                        <CheckboxMui label="Are you a returning student?" ref={returningStudentRef}/>
+                    </RowMui>
+
+                    <br/>
+
+                    <RowMui>
+                        <ButtonMui text="Submit" onClick={handleSubmit}/>
+                    </RowMui>
                 </Grid>
 
-                <Grid item xs={6}>
-                    <InputMui label="age" type="number" InputLabelProps={{shrink: true}} ref={ageRef}/>
-                </Grid>
-            </RowMui>
-
-            <RowMui><CheckboxMui label="Are you a returning student?" ref={returningStudentRef}/></RowMui>
-
-            <RowMui style={{paddingLeft: '1rem'}}>
-                <TreeSelectMui
-                    defaultExpandAll
-                    InputLabelProps={{shrink: true}}
-                    label="Class"
-                    labelKey="nodeLabel"
-                    name="schoolClass"
-                    nestedTree={optionTree}
-                    valueKey="code"
-                    setValue={setValue}
-                    uniqueKey="code"
-                />
-            </RowMui>
-
-            <RowMui>
-                <ButtonMui text="Submit" onClick={handleSubmit}/>
-            </RowMui>
-        </Grid>
-
-    </div>)
+            </div>
+            <SnackbarMui
+                ref={snackbarRef}
+                severity={isSuccess ? 'success' : 'error'}
+            />
+        </>
+    )
 }
 
 export default App
